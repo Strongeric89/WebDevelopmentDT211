111cdcff4332144da0eacc5edd31d4ce92240434
Strongeric89 <ericstrong89@gmail.com>
2017-03-08 11:26:04 +0000

    php stuff

---
+++ phpscripts/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title><?php
+$pagename = "my first php";
+  print($pagename); ?></title>
+</head>
+<body>
+
+  <?php
+  $name = "eric strong";
+   print("Hello $name");
+   echo "<br><i>$name</i>";
+   ?>
+
+</body>
+</html>
